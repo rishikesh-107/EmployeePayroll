73cfe49d5155a2972653929749456ed88e5b1e9d
rishikesh-107 <126221657+rishikesh-107@users.noreply.github.com>
2023-03-01 11:36:29 +0530

    Delete EmployeeMicroservice directory

--- EmployeeMicroservice/src/main/java/com/example/DAO/EmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.DAO;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.entity.EmployeeEntity;
-
-public interface EmployeeDAO extends MongoRepository<EmployeeEntity,Integer>{
-
-}
